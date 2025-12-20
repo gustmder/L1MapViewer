4d21a5be81f09de872eee46d2a24c6b122207780
tonyq <tonylovejava@gmail.com>
2025-12-20 13:55:29 +0800

    升級到 dotnet 10

--- Program.cs
+++ Program.cs
@@ -1,16 +1,27 @@
 using System.Text;
 using L1FlyMapViewer;
 using L1MapViewer;
+using L1MapViewer.CLI;
+using System.Text;
 
 namespace L1MapViewerCore;
 
 static class Program
 {
     [STAThread]
-    static void Main()
+    static int Main(string[] args)
     {
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        // 檢查是否為 CLI 模式
+        if (args.Length > 0 && args[0].ToLower() == "-cli")
+        {
+            return CliHandler.Execute(args);
+        }
+
+        // GUI 模式
         ApplicationConfiguration.Initialize();
         Application.Run(new MapForm());
+        return 0;
     }
 }
